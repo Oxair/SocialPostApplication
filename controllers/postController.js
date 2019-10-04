600328e3b880c50b049dbe3db4f7eae843bee507
Oxair <pk.ozair@gmail.com>
2019-10-04 19:50:34 +0500

    Stage 3 (User D, Post Section U)

---
+++ controllers/postController.js
@@ -0,0 +1,23 @@
+const Post = require('../models/Post')
+
+exports.viewCreateSCreen = function (req, res) {
+    res.render('create-post')
+}
+
+exports.createPost = function (req, res) {
+    let post = new Post(req.body, req.session.user._id)
+    post.create().then(function() {
+        res.send("New post created")
+    }).catch(function(errors) {
+        res.send(errors)
+    })
+}
+
+exports.viewSingle = async function(req, res) {
+  try {
+    let post = await Post.findSingleById(req.params.id)
+    res.render('single-post-screen', {post: post})
+  } catch {
+    res.render('404')
+  }
+}
--- router.js
+++ router.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
+// User routers
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+// Post routers
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateSCreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.createPost)
+router.get('/post/:id', postController.viewSingle)
 
 module.exports = router
